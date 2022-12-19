64b5c1fd29c24c4988d7316511c3db292b6fe6de
Ayush Singh <as54321303@gmail.com>
2022-12-19 16:46:10 +0530

    Command Line Argument

---
+++ data.js
@@ -0,0 +1,8 @@
+const data=[
+    {name:'Ayush Singh',Age:'21',contact:'9889055589'},
+    {name:'Mahesh Singh',Age:'29',contact:'7355128756'},
+    {name:'Sadhana Singh',Age:'19',contact:'9336222867'},
+    {name:'Lalita Singh',Age:'58',contact:'9918156345'},
+]
+
+module.exports=data;
--- index.js
+++ index.js
@@ -1,3 +1,23 @@
-const colors=require('colors');
+const fs=require('fs');
+// const http=require('http');
+// const data=require('./data');
+// http.createServer((req,res)=>{
+//      res.writeHead(200,{'Content-Type':'application/json'});
+//      res.write(JSON.stringify(data));
+//      res.end();
+// }).listen(8000); 
 
-console.log("Ayush".orange)
+const input=process.argv;
+
+if(input[2]=='add')
+{
+    fs.writeFileSync(input[3],input[4]);
+}else if(input[2]=='remove')
+{
+    fs.unlinkSync(input[3]);
+}
+
+else
+{
+    console.log('Invalid Input');
+}
