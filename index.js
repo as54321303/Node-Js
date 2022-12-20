ff0237e68dfb6c6122386448ef952315e87197b0
Ayush Singh <as54321303@gmail.com>
2022-12-20 13:12:44 +0530

    Asynchronus Concept

--- index.js
+++ index.js
@@ -1,16 +1,7 @@
-const fs=require('fs');
-const path=require('path');
+console.log('first...')
 
-const dirPath=path.join(__dirname,'files')
+setTimeout(()=>{
+      console.log('second...')
+},2000)
 
-// for(i=0;i<5;i++)
-// {
-//     fs.writeFileSync(dirPath+"/"+i+".txt","Text Files");
-// }
-
-
-fs.readdir(dirPath,(err,files)=>{
-    files.forEach((item)=>{
-        console.log('File name is ',item)
-    })
-})
+console.log('third...')
