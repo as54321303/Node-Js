de5c4f4ca66c576a8caf075466279b903d2fc7c1
Ayush Singh <as54321303@gmail.com>
2022-12-20 12:03:39 +0530

    Show File Listing

--- index.js
+++ index.js
@@ -1,23 +1,16 @@
 const fs=require('fs');
-// const http=require('http');
-// const data=require('./data');
-// http.createServer((req,res)=>{
-//      res.writeHead(200,{'Content-Type':'application/json'});
-//      res.write(JSON.stringify(data));
-//      res.end();
-// }).listen(8000); 
+const path=require('path');
 
-const input=process.argv;
+const dirPath=path.join(__dirname,'files')
 
-if(input[2]=='add')
-{
-    fs.writeFileSync(input[3],input[4]);
-}else if(input[2]=='remove')
-{
-    fs.unlinkSync(input[3]);
-}
+// for(i=0;i<5;i++)
+// {
+//     fs.writeFileSync(dirPath+"/"+i+".txt","Text Files");
+// }
 
-else
-{
-    console.log('Invalid Input');
-}
+
+fs.readdir(dirPath,(err,files)=>{
+    files.forEach((item)=>{
+        console.log('File name is ',item)
+    })
+})
